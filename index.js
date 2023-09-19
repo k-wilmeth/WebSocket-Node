b371cf3b0efd1462c03e5b2a63a7ed5f77e3b7c6
Kaipo Wilmeth <kwilmeth@FTS-100555.lan>
2023-09-19 10:44:16 -0500

    Add nvmrc file

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:<PORT>', // Replace <PORT> with the port your front-end httpServer is running on
+  origin: 'http://localhost:3000', // Replace with your front-end client PORT
   methods: ["GET", "POST"]
 }))
 
@@ -18,60 +18,77 @@ const httpServer = require("http").createServer();
 require("./socket")(httpServer);
 
 // If not using rabbitmq, you don't need this.
-const smartGroupId = process.env.SMART_GROUP_ID || "*";
+// const smartGroupId = process.env.SMART_GROUP_ID || "*";
 
 
-let connectedToRabbit = false;
-const connectToRabbitMQAndSendMessages = async (smartGroupId) => {
-  try {
-    const io = require("./socket").getIO();
+// let connectedToRabbit = false;
+// const connectToRabbitMQAndSendMessages = async (smartGroupId) => {
+//   try {
+//     const io = require("./socket").getIO();
 
-    if (!connectedToRabbit) {
-      connection = await amqp.connect(process.env.RABBITMQ_URL);
-      console.log('Connected to RabbitMQ');
-      connectedToRabbit = true;
+//     if (!connectedToRabbit) {
+//       connection = await amqp.connect(process.env.RABBITMQ_URL);
+//       console.log('Connected to RabbitMQ');
+//       connectedToRabbit = true;
     
-      channel = await connection.createChannel();
-      console.log('Created RabbitMQ channel');
-      const exchangeName = 'topic';
-      const routingKey = `dim.*.smartgroups.${smartGroupId}.exports.*`;
+//       channel = await connection.createChannel();
+//       console.log('Created RabbitMQ channel');
+//       const exchangeName = 'topic';
+//       const routingKey = `dim.*.smartgroups.${smartGroupId}.exports.*`;
     
-      await channel.assertExchange(exchangeName, 'topic', { durable: true });
-      const queueResult = await channel.assertQueue('', { exclusive: true });
-      const queueName = queueResult.queue;  
+//       await channel.assertExchange(exchangeName, 'topic', { durable: true });
+//       const queueResult = await channel.assertQueue('', { exclusive: true });
+//       const queueName = queueResult.queue;  
     
-      await channel.bindQueue(queueName, exchangeName, routingKey);
-      console.log('Waiting for messages...');
+//       await channel.bindQueue(queueName, exchangeName, routingKey);
+//       console.log('Waiting for messages...');
     
-      channel.consume(queueName, (message) => {
-        try {
-          const content = message.content.toString();
-          let body = JSON.parse(content);
-          console.log(body?.status + " -> progress: " + body?.progress);
+//       channel.consume(queueName, (message) => {
+//         try {
+//           const content = message.content.toString();
+//           let body = JSON.parse(content);
+//           console.log(body?.status + " -> progress: " + body?.progress);
       
-          /* Send messages to WebSocket */
-          /* NOTE: If you don't need RabbitMQ, just comment out the entire 
-          connectToRabbitMQAndSendMessages method and just use this line replacing the body variable with a test message*/
-          io.sockets.emit("socket_data", body);
+//           /* Send messages to WebSocket */
+//           /* NOTE: If you don't need RabbitMQ, just comment out the entire 
+//           connectToRabbitMQAndSendMessages method and just use this line replacing the body variable with a test message*/
+//           io.sockets.emit("socket_data", body);
     
-          // Acknowledge the message to remove it from the queue
-          channel.ack(message);
-        } catch (error) {
-          console.error('Error processing message:', error);
+//           // Acknowledge the message to remove it from the queue
+//           channel.ack(message);
+//         } catch (error) {
+//           console.error('Error processing message:', error);
       
-          // Reject the message and do not requeue it
-          channel.reject(message, false);
-        }
-      });
+//           // Reject the message and do not requeue it
+//           channel.reject(message, false);
+//         }
+//       });
+//     }
+//   } catch(err) {
+//     console.error('Error On RabbitMQ execution method: ', err);
+//   }
+// }
+
+const sendMessagesToFrontEnd = async () => {
+  const io = require("./socket").getIO();
+  let count = 0;
+  const sendMessages = () => {
+    count++;
+    if (count <= 100) {
+      io.sockets.emit("socket_data", count);
+      console.log(`sending count of ${count} to front-end`)
+    } else {
+      console.log('finish. Restart server to retrigger');
+      clearInterval(msgInterval);
     }
-  } catch(err) {
-    console.error('Error On RabbitMQ execution method: ', err);
   }
+  const msgInterval = setInterval(sendMessages, 50);
+
 }
 
 
 const PORT = 3003;
 httpServer.listen(3003, async () => {
   console.log(`websocket-be is listening on port ${PORT}`)
-  await connectToRabbitMQAndSendMessages(smartGroupId)
+  await sendMessagesToFrontEnd();
 });
